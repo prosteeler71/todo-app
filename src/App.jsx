8281ef0b53cc7937579c9f413d35d85055f1197f
prosteeler71 <cyberpower65@gmail.com>
2023-06-09 22:27:03 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoList from './TodoList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Todo App</h1>
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
+
